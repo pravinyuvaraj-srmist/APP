0224a084c4eaad9089ce25b29ae57b15c04c3167
Pravin Yuvaraj R S <py3900@srmist.edu.in>
2026-07-31 13:25:18 +0530

    Add Rectangle class to calculate area

---
+++ rectanglearea.java
@@ -0,0 +1,17 @@
+class Rectangle {
+    double length;
+    double breadth;
+    void calculateArea() {
+        double area = length * breadth;
+        System.out.println("Rectangle Details");
+        System.out.println("Length  : " + length);
+        System.out.println("Breadth : " + breadth);
+        System.out.println("Area    : " + area);
+    }
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle();
+        r1.length = 12.5;
+        r1.breadth = 8.0;
+        r1.calculateArea();
+    }
+}
