ae276224881a42596ce9c811363683a6ca88e095
Pravin Yuvaraj R S <py3900@srmist.edu.in>
2026-08-14 13:24:47 +0530

    Add Ride class and main method for amusement park

---
+++ amusement.java
@@ -0,0 +1,28 @@
+class Ride {
+    int rideNumber;
+    String rideName;
+    Ride(int rideNumber, String rideName) {
+        this.rideNumber = rideNumber;
+        this.rideName = rideName;
+    }
+    void display() {
+        System.out.print(rideName + " : ");
+        for (int j = 1; j <= rideNumber; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Ride[] rides = new Ride[5];
+        rides[0] = new Ride(1, "Roller Coaster");
+        rides[1] = new Ride(2, "Ferris Wheel");
+        rides[2] = new Ride(3, "Bumper Cars");
+        rides[3] = new Ride(4, "Water Ride");
+        rides[4] = new Ride(5, "Haunted House");
+        for (int i = 0; i < 5; i++) {
+            rides[i].display();
+        }
+    }
+}
