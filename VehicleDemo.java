da31848bbd285738f163abcafc074a403bb8e4d8
Pravin Yuvaraj R S <py3900@srmist.edu.in>
2026-09-17 13:04:30 +0530

    Create VehicleDemo.java

---
+++ VehicleDemo.java
@@ -0,0 +1,54 @@
+class Vehicle {
+    String vehicleNumber;
+    String brand;
+    double speed;
+    Vehicle(String vehicleNumber, String brand, double speed) {
+        this.vehicleNumber = vehicleNumber;
+        this.brand = brand;
+        this.speed = speed;
+    }
+    void displayDetails() {
+        System.out.println("Vehicle Number: " + vehicleNumber);
+        System.out.println("Brand: " + brand);
+        System.out.println("Speed: " + speed + " km/h");
+    }
+}
+class Car extends Vehicle {
+    int numberOfDoors;
+    Car(String vehicleNumber, String brand, double speed, int numberOfDoors) {
+        super(vehicleNumber, brand, speed);
+        this.numberOfDoors = numberOfDoors;
+    }
+    @Override
+    void displayDetails() {
+        System.out.println("\n--- Car Details ---");
+        System.out.println("Vehicle Number: " + vehicleNumber);
+        System.out.println("Brand: " + brand);
+        System.out.println("Speed: " + speed + " km/h");
+        System.out.println("Number of Doors: " + numberOfDoors);
+    }
+}
+class Bike extends Vehicle {
+    boolean hasGear;
+    Bike(String vehicleNumber, String brand, double speed, boolean hasGear) {
+        super(vehicleNumber, brand, speed);
+        this.hasGear = hasGear;
+    }
+    @Override
+    void displayDetails() {
+        System.out.println("\n--- Bike Details ---");
+        System.out.println("Vehicle Number: " + vehicleNumber);
+        System.out.println("Brand: " + brand);
+        System.out.println("Speed: " + speed + " km/h");
+        System.out.println("Has Gear: " + hasGear);
+    }
+}
+public class VehicleDemo {
+    public static void main(String[] args) {
+        Vehicle vehicle;
+        vehicle = new Car("TN01AB1234", "Toyota", 180, 4);
+        vehicle.displayDetails();
+        vehicle = new Bike("TN02XY5678", "Yamaha", 120, true);
+        vehicle.displayDetails();
+    }
+}
